49d8d45636fe06e091c7a09ac8d94bb5de5d7fc9
bharat <bharat2828@gmail.com>
2016-02-11 18:21:18 +0530

    modification

--- GridCount/src/pkg1/Grid.java
+++ GridCount/src/pkg1/Grid.java
@@ -24,6 +24,37 @@ public class Grid {
 					arr[i][j] = sc.nextInt();
 				}
 			}
+		
+		
+			int total=0;
+			
+		
+		
+			int start=0;
+			int end=Ssize;
+			
+			for(int i=start; i<end; i++){
+				for(int j=start; j<end; j++){
+					
+					 total += arr[i][j];
+				}
+				
+			}
+			start +=start;
+			end +=end;
+			
+			if(Lsize >= end){
+			for(int i=start; i<end; i++){
+				for(int j=start ; j<end; j++){
+					
+					 total += arr[i][j];
+				}
+				
+			}
+			
+			}
+			
+		
 		} else {
 			System.out.println("Matrix size must be less then or eqal to 1000");
 		}
